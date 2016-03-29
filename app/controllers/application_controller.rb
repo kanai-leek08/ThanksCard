bbf3de7e1f68c686988c6dd83738a68b3440419e
kanai-leek <kanai.leek08@gmail.com>
2016-03-29 22:57:38 +0900

    public配下のassetsを適切な場所へ移動中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  #herokuに設定しているBasic認証用のENV情報でBasic認証をかける
   http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
